96d3349db64bb3f11f04ccae451d801d62a806ef
luizmarcelloxamarin <luiztelemicro@hotmail.com>
2020-08-23 19:23:09 -0300

    23.08.2020_19:22hs

---
+++ XF_Mvvm_Exercicio2/XF_Mvvm_Exercicio2/ViewModels/AloMvvmViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace XF_Mvvm_Exercicio2.ViewModels
+{
+    public class AloMvvmViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
+
+
---
+++ XF_Mvvm_Exercicio2/XF_Mvvm_Exercicio2/Views/AloMvvPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XF_Mvvm_Exercicio2.ViewModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF_Mvvm_Exercicio2.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AloMvvPage : ContentPage
+    {
+        public AloMvvPage()
+        {
+            InitializeComponent();
+
+            this.BindingContext = new AloMvvmViewModel();
+        }
+    }
+}
